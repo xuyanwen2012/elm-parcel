dafcd3b1336bb5b0e927f2caaef4e5a97d24524a
Yanwen Xu <xuyanwen2012@gmail.com>
2024-06-25 00:04:43 -0700

    s

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, a, div, img, nav, text)
+import Html exposing (Html, a, div, h2, img, li, nav, text, ul)
 import Html.Attributes exposing (class, href, src)
 import Html.Events exposing (onClick)
 
@@ -21,13 +21,28 @@ type NavItem
     | Calendar
 
 
+type alias Paper =
+    { title : String
+    , authors : String
+    , year : Int
+    }
+
+
 type alias Model =
-    { selected : NavItem }
+    { selected : NavItem
+    , papers : List Paper
+    }
 
 
 init : Model
 init =
-    { selected = Dashboard }
+    { selected = Dashboard
+    , papers =
+        [ { title = "Paper A", authors = "Author A", year = 2021 }
+        , { title = "Paper B", authors = "Author B", year = 2022 }
+        , { title = "Paper C", authors = "Author C", year = 2023 }
+        ]
+    }
 
 
 
@@ -49,23 +64,31 @@ update (Select item) model =
 
 view : Model -> Html Msg
 view model =
-    nav [ class "bg-gray-800" ]
-        [ div [ class "mx-auto max-w-7xl px-2 sm:px-6 lg:px-8" ]
-            [ div [ class "relative flex h-16 items-center justify-between" ]
-                [ div [ class "flex flex-1 items-center justify-center sm:items-stretch sm:justify-start" ]
-                    [ div [ class "flex flex-shrink-0 items-center" ]
-                        [-- Logo
-                        ]
-                    , div [ class "hidden sm:ml-6 sm:block" ]
-                        [ div [ class "flex space-x-4" ]
-                            [ navItem model Dashboard "Dashboard"
-                            , navItem model Team "Team"
-                            , navItem model Projects "Projects"
-                            , navItem model Calendar "Calendar"
-                            ]
-                        ]
-                    ]
-                ]
+    -- nav [ class "bg-gray-800" ]
+    --     [ div [ class "mx-auto max-w-7xl px-2 sm:px-6 lg:px-8" ]
+    --         [ div [ class "relative flex h-16 items-center justify-between" ]
+    --             [ div [ class "flex flex-1 items-center justify-center sm:items-stretch sm:justify-start" ]
+    --                 [ div [ class "flex flex-shrink-0 items-center" ]
+    --                     [-- Logo
+    --                     ]
+    --                 , div [ class "hidden sm:ml-6 sm:block" ]
+    --                     [ div [ class "flex space-x-4" ]
+    --                         [ navItem model Dashboard "Dashboard"
+    --                         , navItem model Team "Team"
+    --                         , navItem model Projects "Projects"
+    --                         , navItem model Calendar "Calendar"
+    --                         ]
+    --                     ]
+    --                 ]
+    --             ]
+    --         ]
+    --     ]
+    div [ class "bg-gray-100 min-h-screen flex items-center justify-center p-4" ]
+        [ div [ class "bg-white shadow overflow-hidden sm:rounded-lg w-full max-w-4xl" ]
+            [ div [ class "px-4 py-5 sm:px-6" ]
+                [ h2 [ class "text-lg leading-6 font-medium text-gray-900" ] [ text "Published Papers" ] ]
+            , ul [ class "divide-y divide-gray-200" ]
+                (List.map viewPaper model.papers)
             ]
         ]
 
@@ -84,3 +107,23 @@ navItem model item label =
                 "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
     in
     a [ href "#", class itemClass, onClick (Select item) ] [ text label ]
+
+
+viewPaper : Paper -> Html Msg
+viewPaper paper =
+    li [ class "px-4 py-4 sm:px-6" ]
+        [ div [ class "flex items-center justify-between" ]
+            [ div [ class "text-sm font-medium text-indigo-600 truncate" ] [ text paper.title ]
+            , div [ class "ml-2 flex-shrink-0 flex" ]
+                [ div [ class "bg-green-100 text-green-800 text-xs px-2 inline-flex leading-5 font-semibold rounded-full" ]
+                    [ text (String.fromInt paper.year)
+                    ]
+                ]
+            ]
+        , div [ class "mt-2 sm:flex sm:justify-between" ]
+            [ div [ class "sm:flex" ]
+                [ div [ class "mr-6 flex items-center text-sm text-gray-500" ]
+                    [ text ("by " ++ paper.authors) ]
+                ]
+            ]
+        ]
